6ac3d4a58994b5c3a7d4b3adcef45b942941cb9c
Codewithpabitra <pabitra05.maity@gmail.com>
2025-12-27 21:24:52 +0530

    first commit day01 code

---
+++ src/components/Charts/SparkLine.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SparkLine = () => {
+  return (
+    <div>
+      Spark line 
+    </div>
+  )
+}
+
+export default SparkLine
---
+++ src/pages/Ecommerce.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Ecommerce = () => {
+  return (
+    <div>
+      ecoomerce 
+    </div>
+  )
+}
+
+export default Ecommerce
